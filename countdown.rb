39ecf7257d024e7b23b971a6d24bdd36d4a24c25
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-23 00:08:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,5 +6,11 @@ while countdown > 0
   countdown -= 1
 end
 
-def countdown
+def countdown(countdown_to_midnight)
+  countdown = 10
+
+  while countdown > 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
 end
