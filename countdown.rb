371e6c8be38c3a1d9cfe1ed6a844eba45219aee2
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-23 00:15:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,3 +11,14 @@ def countdown(countdown_to_midnight)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(countdown_to_midnight)
+  countdown = 10
+
+  while countdown > 0
+    sleep(1)
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
